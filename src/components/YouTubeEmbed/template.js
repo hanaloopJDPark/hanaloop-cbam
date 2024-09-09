39a6688f2021862f1213f25e53bbc8b7ecab2932
JD Park <jdpark@Dionysioss-MacBook-Pro.local>
2024-09-09 16:11:14 +0900

    youtube embed schema 수정

--- src/components/YouTubeEmbed/template.js
+++ src/components/YouTubeEmbed/template.js
@@ -3,19 +3,32 @@ export const YouTubeEmbedBlockTemplate = {
   label: "YouTube Embed",
   fields: [
     {
-      name: "title",
-      label: "Title",
-      type: "string",
-    },
-    {
-      name: "url",
-      label: "YouTube URL",
-      type: "string",
-    },
-    {
-      name: "caption",
-      label: "Caption",
-      type: "string",
-    },
-  ],
+      name: "videos",
+      label: "YouTube Embed",
+      type: "object",
+      list: true,
+      ui: {
+        itemProps: (item) => ({
+          label: item.title,
+        }),
+      },
+      fields: [
+        { 
+          name: "title",
+          label: "Title",
+          type: "string",
+        },
+        {
+          name: "url",
+          label: "YouTube URL",
+          type: "string",
+        },
+        {
+          name: "caption",
+          label: "Caption",
+          type: "string",
+        },
+      ]
+    }
+  ]
 };
