b2f8e4dfa75f5bd4af756e29f8889a9ae40d2abb
JD Park <jdpark@Dionysioss-MacBook-Pro.local>
2024-09-10 15:06:50 +0900

    youtube iframe size

--- src/components/YouTubeEmbed/index.jsx
+++ src/components/YouTubeEmbed/index.jsx
@@ -3,22 +3,27 @@ import React from "react";
 export const YouTubeEmbed = ({ data, index }) => {  
   return (
     <section key={index} className="w-screen p-10 flex ">
-      <div className="flex flex-row w-full gap-5 justify-center">
+      <div className="flex lg:flex-row w-full gap-10 justify-center flex-col items-center">
         {
           data.items.map((prop, index) => {
             return (
-              <div>
+              <div className="p-5 border shadow-sm rounded-lg bg-gray-100">
                 <div className="text-center">
-                  <h2>{prop.title}</h2>
+                  <h2 className="text-lg">{prop.title}</h2>
                 </div>
-                <iframe
-                  width="400"
-                  height="480"
-                  src={prop.url}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  title="Embedded youtube"
-                />
+                <div className="w-full min-w-[450px] max-w-[500px]">
+                  <div className="relative w-full overflow-hidden pt-[70%]">
+                    <iframe
+                      className="absolute top-0 left-0 right-0 w-full h-full"
+                      width="400"
+                      height="480"
+                      src={prop.url}
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      title="Embedded youtube"
+                    />
+                  </div>
+                </div>
               </div>
             )
           })
