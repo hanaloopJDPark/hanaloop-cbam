7dc70ecd3543ddfc66055490aaf868f147d44b70
JD Park <jdpark@Dionysioss-MacBook-Pro.local>
2024-09-11 17:47:20 +0900

    CBAM 폴더 추가

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -10,13 +10,11 @@ const Post = ({ image, title, description }) => {
       to={'https://www.hanaloop.com/'}
     >
       <div className="flex flex-col md:w-[350px] w-[250px] bg-gray-50 rounded-lg p-5 hover:bg-blue-100 ease-in-out duration-500 transition-all">
-        {image && (
-          <div className="w-full flex justify-center p-5">
-            <div className="flex justify-center bg-white w-[80%] rounded-lg">
-              <img className={styles.featureSvg} src={image} role="img" />
-            </div>
+        <div className="w-full flex justify-center p-5">
+          <div className="flex justify-center bg-white w-[80%]">
+            <img className="w-full h-full rounded-lg" src={image} role="img" />
           </div>
-        )}
+        </div>
         <div className="text--center padding-horiz--md h-[100px]">
           {title && <h3 className="text-lg">{title}</h3>}
           {description && <p className="text-sm">{description}</p>}
